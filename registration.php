cc5ec906da6ca92bbb2bbc8d054a21374f282d09
CYGNET\niyati.vora <niyati.vora@cygnetinfotech.com>
2022-09-16 14:57:30 +0530

    Register tooltip module with composer

---
+++ registration.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Dividebuy
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the dividebuy.co.uk license that is
+ * available through the world-wide-web at this URL:
+ * https://www.dividebuy.com/LICENSE.txt
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade this extension to newer
+ * version in the future.
+ *
+ * @category    Dividebuy
+ * @package     Dividebuy_Tooltip
+ * @copyright   Copyright (c) Mageplaza (https://www.mageplaza.com/)
+ * @license     https://www.mageplaza.com/LICENSE.txt
+ */
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+    ComponentRegistrar::MODULE,
+    'Dividebuy_Tooltip',
+    __DIR__
+);
